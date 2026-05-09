105461f6bc433401dec9a46d52d06db040e51b04
Botulinus00 <markus.mille@gmail.com>
2026-05-09 17:53:22 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+// Service Worker – SurgBoard
+// Helios Klinikum Erfurt – Offline-fähige PWA
+
+const CACHE_NAME = 'surgboard-v8.1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
